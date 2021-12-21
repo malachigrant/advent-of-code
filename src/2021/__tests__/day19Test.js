e2dac51329850ef859b74d9a4f52b4378d48bcf9
malachigrant <malachimgrant@gmail.com>
2021-12-21 00:43:02 -0400

    Day 19 WOOOOO! I did it! It takes ~700 seconds to run though... I could optimize it a bit, but I won't.

--- src/2021/__tests__/day19Test.js
+++ src/2021/__tests__/day19Test.js
@@ -2,10 +2,10 @@ import { part1, part2 } from '../days/day19.js';
 
 describe('day19', () => {
   it('part 1 returns correct value', () => {
-    expect(part1('test/19.input')).toBe(12345);
+    expect(part1('test/19.input')).toBe(79);
   });
 
-  xit('part 2 returns correct value', () => {
-    expect(part2('test/19.input')).toBe(12345);
+  it('part 2 returns correct value', () => {
+    expect(part2('test/19.input')).toBe(3621);
   });
 });
--- src/2021/days/day19.js
+++ src/2021/days/day19.js
@@ -1,21 +1,5 @@
 import { readNumberInput, readInput } from '../helpers/index.js';
 
-function getBlockDistance([x, y, z], [x2, y2, z2]) {
-  return Math.abs(x - x2) + Math.abs(y - y2) + Math.abs(z - z2);
-}
-
-function getDistance([x,y,z], [x2,y2,z2]) {
-  return Math.pow(x - x2, 2) + Math.pow(y - y2, 2) + Math.pow(z - z2, 2);
-}
-
-function getMinMax([x, y, z], [x2, y2, z2]) {
-  return `${Math.min(Math.abs(x - x2), Math.abs(y - y2), Math.abs(z - z2))},${Math.max(Math.abs(x - x2), Math.abs(y - y2), Math.abs(z - z2))}`;
-}
-
-function getInfo(a, b) {
-  return `${getBlockDistance(a, b)},${getDistance(a, b)},${getMinMax(a, b)}`;
-}
-
 function parseInput(fileName) {
   const arr = readInput(fileName);
   const scanners = [];
@@ -94,203 +78,67 @@ function findUniqueBeacons(scanners, scannerPositions) {
       found[coordStr] = true;
     });
   });
+  return count;
+}
+
+function transformAndLocateScanners(scanners) {
+  const unknownScanners = [...scanners.slice(1).map((_, i) => i + 1)];
+  console.log(unknownScanners);
+  const knownScannerPositions = [[0,0,0]]; // scanner 0 is the center point
+  const knownsChecked = [];
+  while (unknownScanners.length > 0) {
+    for (let j = 0; j < knownScannerPositions.length; j++) {
+      if (!knownScannerPositions[j] || knownsChecked[j]) {
+        continue;
+      }
+      knownsChecked[j] = true;
+      const knownScanner = scanners[j];
+      for (let i = 0; i < unknownScanners.length; i++) {
+        const unknownScanner = scanners[unknownScanners[i]];
+        transformFormats.some(transform => {
+          return transformMultipliers.some(multiplier => {
+            return knownScanner.some(knownCoords => {
+              return unknownScanner.some(unknownCoords => {
+                const diff = subVec(knownCoords, transformCoords(unknownCoords, transform, multiplier));
+                const translatedUnknownScanner = unknownScanner.map(coords => addVec(diff, transformCoords(coords, transform, multiplier)));
+                const overlap = findOverlap(knownScanner, translatedUnknownScanner);
+                if (overlap >= 12) {
+                  knownScannerPositions[unknownScanners[i]] = addVec(diff, knownScannerPositions[j]);
+                  unknownScanner.forEach((coord, coordIndex) => {
+                    unknownScanner[coordIndex] = transformCoords(coord, transform, multiplier);
+                  });
+                  unknownScanners.splice(i, 1);
+                  i--;
+                  return true;
+                }
+              });
+            });
+          });
+        });
+      }
+    }
+  }
+  return knownScannerPositions;
 }
 
 export function part1(fileName) {
   const scanners = parseInput(fileName);
-  const unknownScanners = [...scanners.slice(1).map((_, i) => i + 1)];
-  console.log(unknownScanners);
-  const knownScannerPositions = [[0,0,0]]; // scanner 0 is the center point
-  for (let j = 0; j < knownScannerPositions.length; j++) {
-    if (!knownScannerPositions[j]) {
-      continue;
-    }
-    const knownScanner = scanners[j];
-    for (let i = 0; i < unknownScanners.length; i++) {
-      const unknownScanner = scanners[unknownScanners[i]];
-      console.log('unknown', unknownScanners[i], 'known', j);
-      transformFormats.some(transform => {
-        return transformMultipliers.some(multiplier => {
-          return knownScanner.some(knownCoords => {
-            return unknownScanner.some(unknownCoords => {
-              const diff = subVec(knownCoords, transformCoords(unknownCoords, transform, multiplier));
-              const translatedUnknownScanner = unknownScanner.map(coords => addVec(diff, transformCoords(coords, transform, multiplier)));
-              const overlap = findOverlap(knownScanner, translatedUnknownScanner);
-              if (overlap >= 12) {
-                console.log(i, j, overlap, addVec(diff, knownScannerPositions[j]));
-                knownScannerPositions[unknownScanners[i]] = addVec(diff, knownScannerPositions[j]);
-                unknownScanner.forEach((coord, coordIndex) => {
-                  unknownScanner[coordIndex] = transformCoords(coord, transform, multiplier);
-                });
-                unknownScanners.splice(i, 1);
-                console.log(unknownScanners, i, j);
-                i--;
-                return true;
-              }
-            });
-          });
-        });
-      });
-    }
-  }
-  console.log(knownScannerPositions);
+  const knownScannerPositions = transformAndLocateScanners(scanners);
   return findUniqueBeacons(scanners, knownScannerPositions);
 }
 
-export function old_part1(fileName) {
-  const scanners = parseInput(fileName);
-  const scannerDistancesMap = [];
-  scanners.forEach((scanner, i) => {
-    const currentScannerDistMap = [];
-    scannerDistancesMap[i] = currentScannerDistMap;
-    for (let first = 0; first < scanner.length - 1; first++) {
-      const firstBeaconCoords = scanner[first];
-      currentScannerDistMap[first] = currentScannerDistMap[first] || [];
-      for (let other = first + 1; other < scanner.length; other++) {
-        const otherBeaconCoords = scanner[other];
-        currentScannerDistMap[other] = currentScannerDistMap[other] || [];
-        currentScannerDistMap[first][other] = getInfo(firstBeaconCoords, otherBeaconCoords);
-        //currentScannerDistMap[other][first] = currentScannerDistMap[first][other];
-      }
-    }
-  });
-
-  const betweenScannerMappings = {};
-  // check how many beacons are same distance between s1 and s2
-  for (let i = 0; i < scanners.length - 1; i++) {
-    for (let j = i + 1; j < scanners.length; j++) {
-      const similarMap = {};
-      const distMap1 = scannerDistancesMap[i];
-      const distMap2 = scannerDistancesMap[j];
-      // console.log(i, j, distMap1);
-      for (let firstScannerFirstBeaconIndex = 0; firstScannerFirstBeaconIndex < distMap1.length; firstScannerFirstBeaconIndex++) {
-        if (distMap1[firstScannerFirstBeaconIndex] === undefined) {
-          continue;
-        }
-        for (let firstScannerSecondBeaconIndex = 0; firstScannerSecondBeaconIndex < distMap1[firstScannerFirstBeaconIndex].length; firstScannerSecondBeaconIndex++) {
-          if (distMap1[firstScannerFirstBeaconIndex][firstScannerSecondBeaconIndex] === undefined) {
-            continue;
-          }
-          for (let secondScannerFirstBeaconIndex = 0; secondScannerFirstBeaconIndex < distMap2.length; secondScannerFirstBeaconIndex++) {
-            if (distMap2[secondScannerFirstBeaconIndex] === undefined) {
-              continue;
-            }
-            for (let secondScannerSecondBeaconIndex = 0; secondScannerSecondBeaconIndex < distMap2[secondScannerFirstBeaconIndex].length; secondScannerSecondBeaconIndex++) {
-              if (distMap2[secondScannerFirstBeaconIndex][secondScannerSecondBeaconIndex] === undefined) {
-                continue;
-              }
-              const dist1 = distMap1[firstScannerFirstBeaconIndex][firstScannerSecondBeaconIndex];
-              const dist2 = distMap2[secondScannerFirstBeaconIndex][secondScannerSecondBeaconIndex];
-              if (dist1 === dist2) {
-                // console.log('same dist', dist1);
-                similarMap[`${firstScannerFirstBeaconIndex},${firstScannerSecondBeaconIndex}`] = `${secondScannerFirstBeaconIndex},${secondScannerSecondBeaconIndex},${dist1}`;
-              }
-            }
-          }
-        }
-      }
-      const beaconMappings = {};
-      Object.keys(similarMap).forEach(key => {
-        const [left1, left2] = key.split(',').map(Number);
-        const [right1, right2] = similarMap[key].split(',').map(Number);
-        beaconMappings[left1] = beaconMappings[left1] || {};
-        beaconMappings[left2] = beaconMappings[left2] || {};
-        beaconMappings[left1][right1] = (beaconMappings[left1][right1] || 0) + 1;
-        beaconMappings[left2][right2] = (beaconMappings[left2][right2] || 0) + 1;
-      });
-      const finalMapping = {};
-      Object.keys(beaconMappings).forEach(key => {
-        const mapping = Object.keys(beaconMappings[key]).sort((a, b) => beaconMappings[key][b] - beaconMappings[key][a]);
-        finalMapping[key] = mapping[0];
-        if (i === 1 && j === 4) {
-          console.log(beaconMappings);
-        }
-      });
-      betweenScannerMappings[`${i},${j}`] = finalMapping;
-      // console.log(i, j, finalMapping);
-    }
-  }
-  // console.log(betweenScannerMappings);
-  const transformFormats = [
-    [0, 1, 2],
-    [0, 2, 1],
-    [1, 0, 2],
-    [1, 2, 0],
-    [2, 0, 1],
-    [2, 1, 0],
-  ];
-  const transformMultipliers = [
-    [1, 1, 1],
-    [1, 1, -1],
-    [1, -1, 1],
-    [1, -1, -1],
-    [-1, 1, 1],
-    [-1, 1, -1],
-    [-1, -1, 1],
-    [-1, -1, -1],
-  ];
-  const finalSolutionMap = {};
-  const scannerPositions = [[0,0,0]];
-  scanners[0].forEach(val => {
-    finalSolutionMap[`${val[0]},${val[1]},${val[2]}`] = true;
-  });
-  for (let i = 0; i < scanners.length - 1; i++) {
-    for (let j = i + 1; j < scanners.length; j++) {
-      if (betweenScannerMappings[`${i},${j}`] === undefined || Object.keys(betweenScannerMappings[`${i},${j}`]).length < 12) {
-        continue;
-      }
-      const debugMap = betweenScannerMappings[`${i},${j}`];
-      const coordMap = {};
-      Object.keys(debugMap).forEach(key => {
-        const tempBeaconIndex = debugMap[key];
-        coordMap[scanners[i][key]] = scanners[j][tempBeaconIndex];
-      });
-      let finalDiff;
-      let correctTransformFormat;
-      let correctMultiplierFormat;
-      transformFormats.some(transform => {
-        return transformMultipliers.some(multiplier => {
-          let diff;
-          if (Object.keys(coordMap).every(key => {
-            const coords1 = key.split(',').map(Number);
-            const coords2 = transform.map((val, i) => coordMap[key][val] * multiplier[i]);
-            const currentDiff = `${coords1[0] - coords2[0]},${coords1[1] - coords2[1]},${coords1[2] - coords2[2]}`;
-            if (!diff) {
-              diff = currentDiff;
-              return true;
-            } else {
-              return diff === currentDiff;
-            }
-          })) {
-            console.log('found', i, j);
-            finalDiff = diff;
-            console.log(scannerPositions[i]);
-            scannerPositions[j] = finalDiff.split(',').map(Number).map((num, index) => num - (scannerPositions[i] ? scannerPositions[i][index] : 0));
-            correctMultiplierFormat = multiplier;
-            correctTransformFormat = transform;
-            return true;
-          } else {
-            // console.log('failed', i, j);
-          }
-        });
-      });
-      if (i === 0 && j === 1) {
-        scanners[j].map(val => {
-          const transformedCoords = correctTransformFormat.map((tVal, i) => val[tVal] * correctMultiplierFormat[i]);
-          finalSolutionMap[`${transformedCoords[0] + scannerPositions[j][0]},${transformedCoords[1] + scannerPositions[j][1]},${transformedCoords[2] + scannerPositions[j][2]}`] = true;
-          return transformedCoords;
-        });
-      }
-    }
-  }
-
-  console.log(scannerPositions);
-  
-
-  console.log(Object.keys(finalSolutionMap).length);
+function getManhattanDistance(a, b) {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]) + Math.abs(a[2] - b[2]);
 }
 
 export function part2(fileName) {
-  const arr = readInput(fileName);
-
+  const scanners = parseInput(fileName);
+  const scannerPositions = transformAndLocateScanners(scanners);
+  let maxManhattanDistance = 0;
+  for (let i = 0; i < scannerPositions.length - 1; i++) {
+    for (let j = i + 1; j < scannerPositions.length; j++) {
+      maxManhattanDistance = Math.max(maxManhattanDistance, getManhattanDistance(scannerPositions[i], scannerPositions[j]));
+    }
+  }
+  return maxManhattanDistance;
 }
