4601a6c3aed7be0d3d857d5fc1b9807a822a5f6f
malachigrant <malachimgrant@gmail.com>
2021-12-24 00:31:52 -0400

    Day 23 WIP

---
+++ src/2021/__tests__/day23Test.js
@@ -0,0 +1,11 @@
+import { part1, part2 } from '../days/day23.js';
+
+describe('day23', () => {
+  it('part 1 returns correct value', () => {
+    expect(part1('test/23.input')).toBe(12521);
+  });
+
+  xit('part 2 returns correct value', () => {
+    expect(part2('test/23.input')).toBe(12345);
+  });
+});
